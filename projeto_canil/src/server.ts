07fb91be48ed3f613f5fa824f4d8517dac76a193
Alexander Alves <vendas77.rj@gmail.com>
2023-05-04 06:44:31 -0300

    Atualizando o arquivo server

--- projeto_canil/src/server.ts
+++ projeto_canil/src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 const server = express();
@@ -12,7 +13,11 @@ server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')))
 
+server.use(mainRoutes);
 
+server.use((req: Request, res:Response) => {
+  res.send("404 - Página não encontrada.");
+});
 
 
 server.listen(process.env.PORT, () => console.log('Servidor rodando na porta 4000.'));
