944640b9aa2904ee1758b3611b5b43c0eca82e30
Alexander Alves <vendas77.rj@gmail.com>
2023-05-10 11:50:25 -0300

    Tipos de matches

--- node_teste_tdd/src/libs/Math.test.ts
+++ node_teste_tdd/src/libs/Math.test.ts
@@ -1,6 +1,8 @@
+import exp from 'constants';
 import { Math } from './Math';
 
 describe('Testing the library Math', () => {
+
   it('should sum two numbers correctly', () => {
     const response = Math.sum(5, 10);
     expect(response).toBe(15);
@@ -19,5 +21,40 @@ describe('Testing the library Math', () => {
   it('should multiply two numbers correctly', () => {
     const response = Math.mut(3, 3);
     expect(response).toBe(9);
+  });
+
+  it('contar quantos caracteres tem na string', () => {
+    const response = 'Alexander';
+    expect(response).toHaveLength(9);
+  });
+
+  it('se possui a propriedade email', () => {
+    const response = { name: 'Alexander', email: 'aeca@gmail.com' };
+    expect(response).toHaveProperty('email');
+  });
+
+  it('se possui certo valor maior que', () => {
+    const response = 20;
+    expect(response).toBeGreaterThan(15);
+  });
+
+  it('se possui certo valor maior ou igual que', () => {
+    const response = 30;
+    expect(response).toBeGreaterThanOrEqual(30);
+  });
+
+  it('se possui certo valor menor que', () => {
+    const response = 40;
+    expect(response).toBeLessThan(50);
+  });
+
+  it('se possui certo valor menor ou igual que', () => {
+    const response = 99;
+    expect(response).toBeLessThanOrEqual(100);
+  });
+
+  it('verificar o email se é válido', () => {
+    const response = 'aeca@gmail.com';
+    expect(response).toMatch(/[a-z]@[a-z].[a-z]/);
   });
 });
