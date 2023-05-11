ebdbd42b08895ea9942b44b125b8e6da276765e3
Alexander Alves <vendas77.rj@gmail.com>
2023-05-11 09:00:43 -0300

    Criando as rotas

--- projeto_olx_node_api/server.js
+++ projeto_olx_node_api/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileupload = require('express-fileupload')
 
+const apiRoutes = require('./src/routes')
+
 mongoose.connect(process.env.DATABASE, {
   maxPoolSize: 50,
   wtimeoutMS: 2500
@@ -22,8 +24,6 @@ server.use(fileupload())
 
 server.use(express.static(__dirname+ '/public'))
 
-server.get('/ping', (req, res) => {
-  res.json({ pong: true })
-})
+server.use('/', apiRoutes)
 
 server.listen(process.env.PORT, () => console.log('- Rodando na porta 5000.'))
