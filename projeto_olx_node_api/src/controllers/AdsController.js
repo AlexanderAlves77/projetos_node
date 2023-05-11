7093b700c38094e77ed88b655d30ec20ae5f9231
Alexander Alves <vendas77.rj@gmail.com>
2023-05-11 12:33:16 -0300

    Listando anuncios

--- projeto_olx_node_api/src/controllers/AdsController.js
+++ projeto_olx_node_api/src/controllers/AdsController.js
@@ -92,7 +92,53 @@ module.exports = {
     res.json({ id: info._id })
   },
   getList: async (req, res) => {
+    let { sort = 'asc', offset = 0, limit = 8, q, cat, state } = req.query
+    let filters = { status: true }
+    let total = 0
 
+    if(q) filters.title = {'$regex': q, 'options': 'i' }
+    if(cat) {
+      const c = await Category.findOne({ slug: cat }).exec()
+      if(c) {
+        filters.category = c._id.toString()
+      }
+    }
+    if(state) {
+      const s = await State.findOne({ name: state.toUpperCase() }).exec()
+      if(s) {
+        filters.state = s._id.toString()
+      }
+    }
+
+    const adsTotal = await Ad.find(filter).exec()
+    total = adsTotal.length 
+
+    const adsData = await Ad.find(filters)
+      .sort({ dateCreated: (sort=='desc' ? -1 : 1) })
+      .skip(parseInt(offset))
+      .limit(parseInt(limit))
+      .exec()
+
+    let ads = []
+
+    for(let i in adsData) {
+      let image;
+      let defaultImage = adsData[i].images.find(e => e.default) 
+      if(defaultImage) {
+        image = `${process.env.BASE}/media/${defaultImage.url}`
+      } else {
+        image = `${process.env.BASE}/media/default.jpg`
+      }
+
+      ads.push({
+        id: adsData[i]._id,
+        title: adsData[i].title,
+        price: adsData[i].price,
+        priceNegotiable: adsData[i].priceNegotiable,
+        image: image,
+      })
+    }
+    res,json({ ads: ads, total: total })
   },
   getItem: async (req, res) => {
 
