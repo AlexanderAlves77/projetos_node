c4939de0c01d93cc4333f32a5ae21bef56d134cb
Alexander Alves <vendas77.rj@gmail.com>
2023-05-11 11:27:40 -0300

    Listando as categorias

--- projeto_olx_node_api/src/controllers/AdsController.js
+++ projeto_olx_node_api/src/controllers/AdsController.js
@@ -1,6 +1,26 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { validationResult, matchedData } = require('express-validator')
+const Category = require('../models/Category')
+const User = require('../models/User')
+const State = require('../models/State')
+const Ad = require('../models/Ad')
+
 module.exports = {
   getCategories: async (req, res) => {
+    const cats = await Category.find()
+    let categories = []
 
+    for(let i in cats) {
+      categories.push({
+        id: cats[i]._id,
+        name: cats[i].name,
+        slug: cats[i].slug,
+        image: `${process.env.BASE}/assets/images/${cats[i].slug}.png`,
+      })
+    }
+
+    res.json({ categories: categories })
   },
   addAction: async (req, res) => {
 
