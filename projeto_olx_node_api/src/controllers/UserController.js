25983c941b1c230ee29b5914d92b7c8c748109c1
Alexander Alves <vendas77.rj@gmail.com>
2023-05-11 10:42:29 -0300

    Pegando informações do usuário

--- projeto_olx_node_api/src/controllers/UserController.js
+++ projeto_olx_node_api/src/controllers/UserController.js
@@ -1,4 +1,7 @@
 const State = require('../models/State')
+const User = require('../models/User')
+const Category = require('../models/Category')
+const Ad = require('../models/Ad')
 
 module.exports = {
   getStates: async (req, res) => {
@@ -6,7 +9,34 @@ module.exports = {
     res.json({ states: states })
   },
   info: async (req, res) => {
+    let token = req.query.token 
+    const user = await User.findOne({ token: token })
+    const state = await State.findById(user.state)
+    const ads = await Ad.find({ idUser: user._id.toString() })
+    let adList = []
 
+    for(let i in ads) {
+      const cat = await Category.findById(ads[i].category)
+      adList.push({
+        id: ads[i]._id,
+        status: ads[i].status,
+        images: ads[i].images,
+        dateCreated: ads[i].dateCreated,
+        title: ads[i].title,
+        price: ads[i].price,
+        priceNegotiable: ads[i].priceNegotiable,
+        description: ads[i].description,
+        views: ads[i].views,
+        category: cat.slug
+      })
+    }
+    
+    res.json({
+      name: user.name,
+      email: user.name,
+      state: state.name,
+      ads: adList 
+    })
   },
   editAction: async (req, res) => {
 
