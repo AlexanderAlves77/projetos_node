48f60c099c21d8418d16356477f3592945274deb
Alexander Alves <vendas77.rj@gmail.com>
2023-05-11 12:09:03 -0300

    Adicionando novo anúncio

--- projeto_olx_node_api/src/controllers/AdsController.js
+++ projeto_olx_node_api/src/controllers/AdsController.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const { validationResult, matchedData } = require('express-validator')
+const { v4: uuid } = require('uuid')
+const jimp = require('jimp')
 const Category = require('../models/Category')
 const User = require('../models/User')
 const State = require('../models/State')
 const Ad = require('../models/Ad')
+
+const addImage = async (buffer) => {
+  let newName = `${uuid()}.jpg`
+  let tmpImg = await jimp.read(buffer)
+  tmpImg.cover(500, 500).quality(80).write(`./public/media/${newName}`)
+  return newName
+}
 
 module.exports = {
   getCategories: async (req, res) => {
@@ -23,7 +32,64 @@ module.exports = {
     res.json({ categories: categories })
   },
   addAction: async (req, res) => {
+    let { title, price, priceneg, desc, cat, token } = req.body
+    const user = await User.findOne({ token: token }).exec()
 
+    if(!title || !cat) {
+      res.json({ error: 'Título ou categoria não foram preenchidos' })
+      return
+    }
+
+    if(price) {
+      price = price.replace('.', '').replace(',','.').replace('R$ ', '')
+      price = parseFloat(price)
+    } else {
+      price = 0
+    }
+
+    const newAd = new Ad()
+    newAd.status = true
+    newAd.id = user._id 
+    newAd.state = user.state
+    newAd.dateCreated = new Date()
+    newAd.title = title
+    newAd.category = cat
+    newAd.price = price
+    newAd.priceNegotiable = (priceneg=='true') ? true : false
+    newAd.descripton = desc
+    newAd.views = 0
+
+    if(req.files && req.files.img) {
+      if(req.files.img.length == undefined) {
+        let imageTypes = ['image/jpeg', 'image/jpg', 'image.png']
+
+        if(imageTypes.includes(req.files.img.mimetype)) {
+          let url = await addImage(req.files.img.data)
+          newAd.images.push({
+            url: url,
+            default: false
+          })
+        }
+
+      } else {
+        for(let i = 0; i < req.files.img.length; i++) {
+          let imageTypes = ['image/jpeg', 'image/jpg', 'image.png']
+
+          if(imageTypes.includes(req.files.img[i].mimetype)) {
+            let url = await addImage(req.files.img[i].data)
+            newAd.images.push({
+              url: url,
+              default: false
+            })
+          }
+        }
+      }
+    }
+
+    if(newAd.images.length > 0) newAd.images[0].default = true
+
+    const info = await newAd.save()
+    res.json({ id: info._id })
   },
   getList: async (req, res) => {
 
